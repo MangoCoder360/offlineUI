c24f7a891b2aae4dc5911f217873e7e82a2bd9d5
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2023-02-10 20:52:39 -0800

    add footer

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+// Splash text
 function pickSplash(){
     var splashText = document.getElementById("splashText");
-    var splashTexts = ["deez nuts","idk, seems kinda suspect","also try cuberacers","not blocked","made with unity","ha you already know"];
+    var splashTexts = ["deez nuts","idk, seems kinda suspect","also try cuberacers","not blocked","made with unity","ha you already know","kyle is sus","9/11"];
     splashText.innerHTML = splashTexts[Math.floor(Math.random()*splashTexts.length)];
 }
-setTimeout(pickSplash,200);
+setTimeout(pickSplash,300);
+
+
+// Tip of the day
+function showToast(){
+    var toastRef = document.getElementById('tipOfTheDay');
+    var toast = new bootstrap.Toast(toastRef);
+    toast.show();
+}
+//setTimeout(showToast,5000);
